8d4a52ff8f41da2ca52bf23ed7e05f296bfc7e2e
Gleb Smirnov <sterlopus@gmail.com>
2021-09-29 20:21:10 +0300

    first commit

---
+++ src/main/java/ru/geekbrains/level3/App.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.level3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
